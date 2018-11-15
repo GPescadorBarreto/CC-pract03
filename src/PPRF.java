9ac33c174e479dc4f57a0c222966c6ae223b125d
GPescadorBarreto <GPescadorBarreto@github.com>
2018-11-15 00:09:56 +0000

    Final Commit

---
+++ src/PPRF.java
@@ -0,0 +1,71 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PPRF.
+ */
+public class PPRF {
+	
+	/**
+	 * Zero.
+	 *
+	 * @return the int
+	 */
+	public static int zero() {
+		return 0;
+	}
+
+	/**
+	 * Next number after x.
+	 *
+	 * @param x the x
+	 * @return the 
+	 */
+	public static int next(int x) {
+		return x + 1;
+	}
+
+	/**
+	 * Adds x and y.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return the int
+	 */
+	public static int add(int x, int y) {
+		if (y == 0) {
+			return x;
+		} else {
+			return next(add(x, y - 1));
+		}
+	}
+
+	/**
+	 * Multiply x by y.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return the int
+	 */
+	public static int multiply(int x, int y) {
+		if (y == 0) {
+			return zero();
+		} else {
+			return add(x, multiply(x, y - 1));
+		}
+	}
+
+	/**
+	 * Power of x to the y.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return the int
+	 */
+	public static int power(int x, int y) {
+		if (y == 0) {
+			return next(zero());
+		} else {
+			return multiply(x, power(x, y - 1));
+		}
+	}
+
+}
---
+++ src/PPRF_Tester.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PPRF_Tester.
+ */
+public class PPRF_Tester {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		String xString;
+		String yString;
+		if (args.length == 0) {
+			System.out.println("Insert the X value: ");
+			Scanner scan = new Scanner(System.in);
+			xString = scan.nextLine();
+			System.out.println("Insert the Y value: ");
+			yString = scan.nextLine();
+			scan.close();
+		} else if (args.length == 2) {
+			xString = args[0];
+			yString = args[1];
+		} else
+			throw new IllegalArgumentException("Incorrect program arguments, check documentation");
+
+		int x = Integer.parseInt(xString);
+		int y = Integer.parseInt(yString);
+
+		System.out.println(PPRF.power(x, y));
+	}
+}
